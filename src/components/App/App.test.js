7627d89141256c1cff707ba9402960629bd36b2d
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-08-19 16:19:33 -0600

    Complete test boilerplate

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App", () => {
+  it("should match the snapshot", () => {
+    const wrapper = <App />;
+    expect(wrapper).toMatchSnapshot();
+  });
 });
