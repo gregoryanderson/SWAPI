0f625902d7663ff1554e6833eb4cea2d7df67e89
Alyssa Lundgren <lundgrea@gmail.com>
2019-08-20 17:35:58 -0600

    Set state of person
    Co-authored-by: Gregory Anderson <anderson.gregory.mail@gmail.com>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,7 +19,6 @@ class App extends Component {
         .then(response => response.json())
         .then(data => ({ ...data, personName: person.name, personSpecies: person.species}))
         .then(personWorldData => this.fetchSpeciesInfo(personWorldData))
-        .then(ish => console.log('boo', ish))
         .catch(error => console.log(error))
       })
       return Promise.all(promises)
@@ -28,20 +27,21 @@ class App extends Component {
     fetchSpeciesInfo = (personWorldInfo) => {
       console.log('in the damn species method', personWorldInfo)
       const promises = personWorldInfo.personSpecies.map(species => {
-        fetch(personWorldInfo.personSpecies[0])
+        return fetch(personWorldInfo.personSpecies[0])
         .then(response => response.json())
         .then(data => ({...data, personName: personWorldInfo.personName, personSpecies: data.name, personPlanet: personWorldInfo.name, personPlanetPopulation: personWorldInfo.population, personLanguage: data.language}))
         .catch(error => console.log(error))
-    })
-    Promise.all(promises)
-  }
+      })
+      return Promise.all(promises)
+    }
+    
 
-
-  componentDidMount() {
-    fetch('https://swapi.co/api/people/')
+    componentDidMount() {
+      fetch('https://swapi.co/api/people/')
       .then(response => response.json())
       .then(data => this.fetchWorld(data.results))
-      .then(info => this.setState({info}))
+      // .then(potato => console.log(potato.flat()))
+      .then(info => this.setState({info: info.flat()}))
       .catch(error => console.log(error))
   }
 
