16702becc61b261e1bf9b1fc8661f6d089317b7d
Alyssa Lundgren <lundgrea@gmail.com>
2019-08-20 15:00:22 -0600

    add Vehicle, People and Planets imports into App and revise App to class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Component }from 'react';
+import People from '../People/People'
+import Planets from '../Planets/Planets'
+import Vehicles from '../Vehicles/Vehicles'
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  render() {
+    return (
     <div>
       <People />
       <Planets />
       <Vehicles />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
