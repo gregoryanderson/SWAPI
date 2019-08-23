2b7d6f50610c0be6970335d1d16d9148336205e5
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-08-23 15:56:17 -0600

    Add Selected Card file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
-import React, { Component }from 'react';
-import { Route, NavLink } from 'react-router-dom'
-import Card from '../Card/Card';
-import Scroll from '../Scroll/Scroll';
-import './App.css';
+import React, { Component } from "react";
+import { Route, NavLink } from "react-router-dom";
+import Card from "../Card/Card";
+import Scroll from "../Scroll/Scroll";
+import SelectedCard from "../SelectedCard/SelectedCard";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -11,124 +12,246 @@ class App extends Component {
       people: [],
       planets: [],
       vehicles: [],
-      hasError: '',
+      hasError: "",
       films: [],
       isLoaded: false,
       favorites: []
-    }
+    };
   }
 
-  fetchPlanetInfoForPerson = (peopleInfo) => {
+  fetchPlanetInfoForPerson = peopleInfo => {
     const promises = peopleInfo.map(person => {
-      return fetch (person.homeworld)
+      return fetch(person.homeworld)
         .then(res => res.json())
-        .then(data => ({ ...data, personName: person.name, personSpecies: person.species}))
-        .then(personAndWorldData => this.fetchSpeciesInfoForPerson(personAndWorldData))
-        .catch(err => console.log(err))
-      })
-      return Promise.all(promises)
-    }
-    
-    fetchSpeciesInfoForPerson = (personWorldInfo) => {
-      const promises = personWorldInfo.personSpecies.map(species => {
-        return fetch(personWorldInfo.personSpecies[0])
-        .then(res => res.json())
-        .then(data => ({name: personWorldInfo.personName, personSpecies: data.name, personPlanet: personWorldInfo.name, personPlanetPopulation: personWorldInfo.population, personLanguage: data.language}))
-        .catch(err => console.log(err))
-      })
-      return Promise.all(promises)
-    }
+        .then(data => ({
+          ...data,
+          personName: person.name,
+          personSpecies: person.species
+        }))
+        .then(personAndWorldData =>
+          this.fetchSpeciesInfoForPerson(personAndWorldData)
+        )
+        .catch(err => console.log(err));
+    });
+    return Promise.all(promises);
+  };
 
-    fetchPlanetInfo = (planetInfo) => {
-      const planetPromises = planetInfo.map(planet => {
-        const residents = planet.residents.map(resident => {
-          return fetch(resident)
-            .then(res => res.json())
-            .then(data => data.name)
-            .catch(err => console.log(err))
-          })
-        return Promise.all(residents)
-          .then(names => (
-            {
-            name: planet.name,
-            terrain: planet.terrain,
-            population: planet.population,
-            climate: planet.climate,
-            residents: names
-           }))
-      })
-      return Promise.all(planetPromises)
-    }
+  fetchSpeciesInfoForPerson = personWorldInfo => {
+    const promises = personWorldInfo.personSpecies.map(species => {
+      return fetch(personWorldInfo.personSpecies[0])
+        .then(res => res.json())
+        .then(data => ({
+          name: personWorldInfo.personName,
+          personSpecies: data.name,
+          personPlanet: personWorldInfo.name,
+          personPlanetPopulation: personWorldInfo.population,
+          personLanguage: data.language,
+          type: 'people'
+        }))
+        .catch(err => console.log(err));
+    });
+    return Promise.all(promises);
+  };
 
-    identifyVehicleInfo = (vehicleInfo) => {
-      return vehicleInfo.map(vehicle => {
-        return {
-          name: vehicle.name,
-          model: vehicle.model,
-          vehicleClass: vehicle.vehicle_class,
-          passengers: vehicle.passengers
-        }
-      })
-    }
+  fetchPlanetInfo = planetInfo => {
+    const planetPromises = planetInfo.map(planet => {
+      const residents = planet.residents.map(resident => {
+        return fetch(resident)
+          .then(res => res.json())
+          .then(data => data.name)
+          .catch(err => console.log(err));
+      });
+      return Promise.all(residents).then(names => ({
+        name: planet.name,
+        terrain: planet.terrain,
+        population: planet.population,
+        climate: planet.climate,
+        residents: names,
+        type: 'planets'
+      }));
+    });
+    return Promise.all(planetPromises);
+  };
 
-    componentDidMount() {
-      fetch('https://swapi.co/api/films/')
-        .then(res => res.json())
-        .then(data => this.setState({films: data.results}))
-        .then(fetch('https://swapi.co/api/people/')
-        .then(res => res.json())
-        .then(data => this.fetchPlanetInfoForPerson(data.results))
-        .then(personInfo => this.setState({people: personInfo.flat()})))
-        .then(fetch('https://swapi.co/api/planets/')
-        .then(res => res.json())
-        .then(data => this.fetchPlanetInfo(data.results))
-        .then(planetInfo => this.setState({planets: planetInfo})))
-        .then(fetch('https://swapi.co/api/vehicles/')
-        .then(res => res.json())
-        .then(data => this.identifyVehicleInfo(data.results))
-        .then(vehicleInfo => this.setState({vehicles: vehicleInfo, isLoaded: true})))
-        .catch(err => console.log(err))
-    }
+  identifyVehicleInfo = vehicleInfo => {
+    return vehicleInfo.map(vehicle => {
+      return {
+        name: vehicle.name,
+        model: vehicle.model,
+        vehicleClass: vehicle.vehicle_class,
+        passengers: vehicle.passengers,
+        type: 'vehicles'
+      };
+    });
+  };
 
-    addFavorite = (id, type) => {
-      const favoritedCard = this.state[type].find(card => id.name == card.name)
-      this.setState({favorites: [...this.state.favorites, favoritedCard]})
-    }
-    
-    render() {
-      console.log('favorites:', this.state.favorites)
-      return (
+  componentDidMount() {
+    fetch("https://swapi.co/api/films/")
+      .then(res => res.json())
+      .then(data => this.setState({ films: data.results }))
+      .then(
+        fetch("https://swapi.co/api/people/")
+          .then(res => res.json())
+          .then(data => this.fetchPlanetInfoForPerson(data.results))
+          .then(personInfo => this.setState({ people: personInfo.flat() }))
+      )
+      .then(
+        fetch("https://swapi.co/api/planets/")
+          .then(res => res.json())
+          .then(data => this.fetchPlanetInfo(data.results))
+          .then(planetInfo => this.setState({ planets: planetInfo }))
+      )
+      .then(
+        fetch("https://swapi.co/api/vehicles/")
+          .then(res => res.json())
+          .then(data => this.identifyVehicleInfo(data.results))
+          .then(vehicleInfo =>
+            this.setState({ vehicles: vehicleInfo, isLoaded: true })
+          )
+      )
+      .catch(err => console.log(err));
+  }
+
+  addFavorite = (id, type) => {
+    const favoritedCard = this.state[type].find(card => id.name == card.name);
+    this.setState({ favorites: [...this.state.favorites, favoritedCard] });
+  };
+
+  render() {
+    console.log("favorites:", this.state.favorites);
+    return (
       <main className="app">
         <header className="nav__header">
           <h1 className="nav__h1">S W A P I - B O X</h1>
           <section className="nav__section--links">
-            <NavLink to="/people" className="nav__link">PEOPLE</NavLink>
-            <NavLink to="/planets" className="nav__link">PLANETS</NavLink>
-            <NavLink to="/vehicles" className="nav__link">VEHICLES</NavLink>
+            <NavLink to="/people" className="nav__link">
+              PEOPLE
+            </NavLink>
+            <NavLink to="/planets" className="nav__link">
+              PLANETS
+            </NavLink>
+            <NavLink to="/vehicles" className="nav__link">
+              VEHICLES
+            </NavLink>
             <div className="nav__div--favorites">
-              <NavLink to="/favorites" className="nav__link favorite__link">FAVORITES</NavLink>
-              <img className="favorite__image"src='http://images2.wikia.nocookie.net/__cb20080228205028/starwars/images/thumb/7/71/Redstarbird.svg/1600px-Redstarbird.svg.png' height='20px' width='20px' />
+              <NavLink to="/favorites" className="nav__link favorite__link">
+                FAVORITES
+              </NavLink>
+              <img
+                className="favorite__image"
+                src="http://images2.wikia.nocookie.net/__cb20080228205028/starwars/images/thumb/7/71/Redstarbird.svg/1600px-Redstarbird.svg.png"
+                height="20px"
+                width="20px"
+              />
             </div>
           </section>
         </header>
         <section className="app__section">
-          <Route path="/people" render={ () => <Card data={this.state.people} addFavorite={this.addFavorite} type={'people'}/> } />
-          <Route path="/planets" render={ () => <Card data={this.state.planets} addFavorite={this.addFavorite} type={'planets'}/> } />
-          <Route path="/vehicles" render={ () => <Card data={this.state.vehicles} addFavorite={this.addFavorite} type={'vehicles'}/> } />
-          <Route path="/favorites" render={ () => <Card data={this.state.favorites} addFavorite={this.addFavorite}/> } />
+          <Route
+            exact
+            path="/people"
+            render={() => (
+              <Card
+                data={this.state.people}
+                addFavorite={this.addFavorite}
+                type={"people"}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/planets"
+            render={() => (
+              <Card
+                data={this.state.planets}
+                addFavorite={this.addFavorite}
+                type={"planets"}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/vehicles"
+            render={() => (
+              <Card
+                data={this.state.vehicles}
+                addFavorite={this.addFavorite}
+                type={"vehicles"}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/favorites"
+            render={() => (
+              <Card
+                data={this.state.favorites}
+                addFavorite={this.addFavorite}
+              />
+            )}
+          />
         </section>
-        {!!this.state.films.length && <Scroll film={this.state.films}/>}
+        <section className="selected__section">
+          <Route
+            path="/people/:id"
+            render={({ match }) => {
+              console.log("this is the match:", match);
+              const foundPerson = this.state.people.find(
+                person => person.name === match.params.id
+              );
+              console.log("this is the foundPerson:", foundPerson);
+              return (
+                <SelectedCard
+                  id={foundPerson.id}
+                  name={foundPerson.name}
+                  type={foundPerson.type}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/vehicles/:id"
+            render={({ match }) => {
+              console.log("this is the match:", match);
+              const foundVehicle = this.state.vehicles.find(
+                vehicle => vehicle.name === match.params.id
+              );
+              console.log("this is the foundPerson:", foundVehicle);
+              return (
+                <SelectedCard
+                  id={foundVehicle.id}
+                  name={foundVehicle.name}
+                  type={foundVehicle.type}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/planets/:id"
+            render={({ match }) => {
+              console.log("this is the match:", match);
+              const foundPlanet = this.state.planets.find(
+                planet => planet.name === match.params.id
+              );
+              console.log("this is the foundPlanet:", foundPlanet);
+              return (
+                <SelectedCard
+                  id={foundPlanet.id}
+                  name={foundPlanet.name}
+                  type={foundPlanet.type}
+                />
+              );
+            }}
+          />
+        </section>
+        {!!this.state.films.length && <Scroll film={this.state.films} />}
       </main>
-    )
+    );
   }
+}
 
-  }
-
-
-  //     {/* {this.state.isLoaded && <button className="button--enter">ENTER</button> }  */}
-  //    
-  //       {this.state.isLoaded && <Main people={this.state.people} vehicles={this.state.vehicles} planets={this.state.planets}/>}
- 
-
+//     {/* {this.state.isLoaded && <button className="button--enter">ENTER</button> }  */}
+//
+//       {this.state.isLoaded && <Main people={this.state.people} vehicles={this.state.vehicles} planets={this.state.planets}/>}
 
 export default App;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Card.css';
 
 
@@ -10,23 +11,28 @@ const Card = ({ data, addFavorite, type }) => {
         //   return <p className="resident">{resident || null}</p>
         // })}
         return (
-        <article className="card__article" id={name}>
+        <article className="card__article" id={name} type={type}>
             <header className="card__header">
                 <h3 className="card__h3">{name || null}</h3>
                 <img src='http://images2.wikia.nocookie.net/__cb20080228205028/starwars/images/thumb/7/71/Redstarbird.svg/1600px-Redstarbird.svg.png' height='40px' width='40px' alt='Star Wars Favorite Button' onClick={() => addFavorite({name}, type)}/>
             </header>
             <div className="card__div">
-                <p className="card__p">{personPlanet || null}</p>
-                <p className="card__p">{personSpecies || null}</p>
-                <p className="card__p">{personPlanetPopulation || null}</p>
-                <p className="card__p">{personLanguage || null}</p>
-                <p className="card__p">{model || null }</p>
-                <p className="card__p">{vehicleClass || null}</p>
-                <p className="card__p">{passengers || null}</p>
-                <p className="card__p">{climate || null}</p>
-                <p className="card__p">{population || null}</p>
-                <p className="card__p">{terrain || null}</p>
-                <p className="card__p">{residents || null}</p>
+              <p className="card__p">{personPlanet || null}</p>
+              <p className="card__p">{personSpecies || null}</p>
+              <p className="card__p">{personPlanetPopulation || null}</p>
+              <p className="card__p">{personLanguage || null}</p>
+              <p className="card__p">{model || null }</p>
+              <p className="card__p">{vehicleClass || null}</p>
+              <p className="card__p">{passengers || null}</p>
+              <p className="card__p">{climate || null}</p>
+              <p className="card__p">{population || null}</p>
+              <p className="card__p">{terrain || null}</p>
+              <p className="card__p">{residents || null}</p>
+            </div>
+            <div className="card__div--button">
+              <Link to={`/${type}/${name}`} className="card__link">
+                <button className="card__button">Click for More!</button>
+              </Link>
             </div>
         </article>
     )
