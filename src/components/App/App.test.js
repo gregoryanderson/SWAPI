5c3e9beb74349b1f2a21acaf49b6ee0e59201f58
Alyssa Lundgren <lundgrea@gmail.com>
2019-08-25 09:53:04 -0600

    add router tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import { MemoryRouter } from 'react-router';
 import App from "./App";
+import Card from "../Card/Card"
+import SelectedCard from "../SelectedCard/SelectedCard"
 
 describe("App", () => {
   it("should match the snapshot", () => {
@@ -12,7 +14,7 @@ describe("App", () => {
 
 
 describe("Router", () => {
-  it("should show the People page when people is selected", () => {
+  it("should show the Main page when nothing is selected", () => {
     const wrapper = mount(
       <MemoryRouter initialEntries={[ '/' ]}>
         <App/>
@@ -20,5 +22,41 @@ describe("Router", () => {
     )
     expect(wrapper.find(App)).toHaveLength(1);        
   })
+
+  it("should show the people page when People is selected", () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[ '/people' ]}>
+        <App/>
+      </MemoryRouter>
+    )
+    expect(wrapper.find(Card)).toHaveLength(1);        
+  })
+
+  it("should show the vehicles page when Vehicles is selected", () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[ '/vehicles' ]}>
+        <App/>
+      </MemoryRouter>
+    )
+    expect(wrapper.find(Card)).toHaveLength(1);        
+  })
+
+  it("should show the planets page when Planets is selected", () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[ '/planets' ]}>
+        <App/>
+      </MemoryRouter>
+    )
+    expect(wrapper.find(Card)).toHaveLength(1);        
+  })
+
+  // it("should show a planet's page when a planet card is selected", () => {
+  //   const wrapper = mount(
+  //     <MemoryRouter initialEntries={[ '/planets/C-3PO' ]}>
+  //       <App/>
+  //     </MemoryRouter>
+  //   )
+  //   expect(wrapper.find(SelectedCard)).toHaveLength(1);        
+  // })
 });
   
